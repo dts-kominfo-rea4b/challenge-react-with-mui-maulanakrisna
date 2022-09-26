db10502f30f4c43da2155e9b692a831a5bb420c5
Maulana Krisna Adhitya <maulanakrisna@pln.co.id>
2022-09-27 06:20:42 +0800

    test 2 attempt 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
+import React from 'react';
 import './App.css';
+
 import Header from './components/Header';
+import Contact from './components/Contact';
 
-// Uncomment untuk memuat daftar kontak
-// import contactsJSON from './data/contacts.json';
+import contactsJSON from './data/contacts.json';
+import { useState } from 'react';
+
 const App = () => {
-  // Masukkan Header dan lakukan map untuk Contact ke dalam div App
-  // untuk membuat daftar kontak bisa menggunakan MUI list
-  // https://mui.com/material-ui/react-list/#folder-list
 
-  // Masukkan contacts yang sudah didapat dalam JSON sebagai initial state
-  // Buatlah handler untuk menambahkan kontak baru yang akan dikirim ke ContactForm
+  const [contactList, setContactList] = useState([]);
 
   return (
     <div className="App">
       <Header />
+      <Contact data={contactsJSON} />
     </div>
   );
 };
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,42 @@
 // Contact component dapat berupa MUI ListItem
 // https://mui.com/material-ui/react-list/#folder-list
 import React from 'react';
-
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 // atau langsung tambahkan dengan sx={{}}
 const Contact = ({ data }) => {
     // Contact berisi foto, nama, telepon, dan email
-    return (<></>);
+    return (
+        <List sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
+            {
+               data.map((datum, index) => {
+                    return (
+                        <ListItem key={index}>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <img src={datum.photo} alt={datum.photo}/>
+                                </Avatar>
+
+                            </ListItemAvatar>
+                            <ListItemText primary={datum.name} secondary={
+                                <div>
+                                    <div>{datum.phone}</div>
+                                    <div>{datum.email}</div>
+                                </div>
+                            }/>
+                        </ListItem>
+                    );
+                }
+            
+                )
+            }
+        </List>
+    );
+    
 };
 
 export default Contact;
